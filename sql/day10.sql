12579bb9faab5c860110b4c1427ea7761db6f0d5
mingfei <mingfei.net@gmail.com>
2017-05-26 10:50:09 +0800

    Update

--- sql/day10.sql
+++ sql/day10.sql
@@ -28,3 +28,31 @@ SELECT
   max(sal)
 FROM scott.emp
 GROUP BY JOB, DEPTNO;
+
+SELECT
+  deptno,
+  count(*)
+FROM scott.emp
+GROUP BY DEPTNO
+HAVING avg(sal) > 2000; -- 组检索 where 行检索
+
+SELECT abs(-100);
+SELECT hex(15);
+SELECT ceiling(1.00000001);
+SELECT floor(1.999999999999);
+SELECT round(1.4999);
+SELECT MOD(10, 1);
+SELECT pi();
+SELECT rand();
+SELECT truncate(1234.567, -4);
+
+SELECT ascii('a');
+SELECT bit_length('asdf'); -- 1 byte = 8 bit
+SELECT concat('hello', ', ', 'world', '!');
+SELECT length('中文');
+SELECT locate('e', 'hello');
+SELECT lcase('H'), upper('h'), ucase('h');
+
+
+
+
